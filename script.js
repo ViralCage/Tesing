e06795473581c477843358f7f87a6ab8073f80f1
James Bro <4683175+ViralCage@users.noreply.github.com>
2022-02-19 22:34:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,105 +1,222 @@
-@import url(//fonts.googleapis.com/css?family=Lato:300:400);
+const displacementSlider = function (opts) {
 
-body {
-  margin:0;
-}
+  let vertex = `
+        varying vec2 vUv;
+        void main() {
+          vUv = uv;
+          gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+        }
+    `;
 
-h1 {
-  font-family: 'Lato', sans-serif;
-  font-weight:300;
-  letter-spacing: 2px;
-  font-size:48px;
-}
-p {
-  font-family: 'Lato', sans-serif;
-  letter-spacing: 1px;
-  font-size:14px;
-  color: #333333;
-}
+  let fragment = `
+        
+        varying vec2 vUv;
 
-.header {
-  position:relative;
-  text-align:center;
-  background: linear-gradient(60deg, rgba(84,58,183,1) 0%, rgba(0,172,193,1) 100%);
-  color:white;
-}
-.logo {
-  width:50px;
-  fill:white;
-  padding-right:15px;
-  display:inline-block;
-  vertical-align: middle;
-}
+        uniform sampler2D currentImage;
+        uniform sampler2D nextImage;
 
-.inner-header {
-  height:65vh;
-  width:100%;
-  margin: 0;
-  padding: 0;
-}
+        uniform float dispFactor;
 
-.flex { /*Flexbox for containers*/
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+        void main() {
 
-.waves {
-  position:relative;
-  width: 100%;
-  height:15vh;
-  margin-bottom:-7px; /*Fix for safari gap*/
-  min-height:100px;
-  max-height:150px;
-}
+            vec2 uv = vUv;
+            vec4 _currentImage;
+            vec4 _nextImage;
+            float intensity = 0.3;
 
-.content {
-  position:relative;
-  height:20vh;
-  text-align:center;
-  background-color: white;
-}
+            vec4 orig1 = texture2D(currentImage, uv);
+            vec4 orig2 = texture2D(nextImage, uv);
+            
+            _currentImage = texture2D(currentImage, vec2(uv.x, uv.y + dispFactor * (orig2 * intensity)));
 
-/* Animation */
+            _nextImage = texture2D(nextImage, vec2(uv.x, uv.y + (1.0 - dispFactor) * (orig1 * intensity)));
 
-.parallax > use {
-  animation: move-forever 25s cubic-bezier(.55,.5,.45,.5)     infinite;
-}
-.parallax > use:nth-child(1) {
-  animation-delay: -2s;
-  animation-duration: 7s;
-}
-.parallax > use:nth-child(2) {
-  animation-delay: -3s;
-  animation-duration: 10s;
-}
-.parallax > use:nth-child(3) {
-  animation-delay: -4s;
-  animation-duration: 13s;
-}
-.parallax > use:nth-child(4) {
-  animation-delay: -5s;
-  animation-duration: 20s;
-}
-@keyframes move-forever {
-  0% {
-   transform: translate3d(-90px,0,0);
+            vec4 finalTexture = mix(_currentImage, _nextImage, dispFactor);
+
+            gl_FragColor = finalTexture;
+
+        }
+    `;
+
+  let images = opts.images,image,sliderImages = [];;
+  let canvasWidth = images[0].clientWidth;
+  let canvasHeight = images[0].clientHeight;
+  let parent = opts.parent;
+  let renderWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+  let renderHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+
+  let renderW, renderH;
+
+  if (renderWidth > canvasWidth) {
+    renderW = renderWidth;
+  } else {
+    renderW = canvasWidth;
   }
-  100% { 
-    transform: translate3d(85px,0,0);
-  }
-}
-/*Shrinking for mobile*/
-@media (max-width: 768px) {
-  .waves {
-    height:40px;
-    min-height:40px;
-  }
-  .content {
-    height:30vh;
-  }
-  h1 {
-    font-size:24px;
-  }
-}
+
+  renderH = canvasHeight;
+
+  let renderer = new THREE.WebGLRenderer({
+    antialias: false });
+
+
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setClearColor(0x23272A, 1.0);
+  renderer.setSize(renderW, renderH);
+  parent.appendChild(renderer.domElement);
+
+  let loader = new THREE.TextureLoader();
+  loader.crossOrigin = "anonymous";
+
+  images.forEach(img => {
+
+    image = loader.load(img.getAttribute('src') + '?v=' + Date.now());
+    image.magFilter = image.minFilter = THREE.LinearFilter;
+    image.anisotropy = renderer.capabilities.getMaxAnisotropy();
+    sliderImages.push(image);
+
+  });
+
+  let scene = new THREE.Scene();
+  scene.background = new THREE.Color(0x23272A);
+  let camera = new THREE.OrthographicCamera(
+  renderWidth / -2,
+  renderWidth / 2,
+  renderHeight / 2,
+  renderHeight / -2,
+  1,
+  1000);
+
+
+  camera.position.z = 1;
+
+  let mat = new THREE.ShaderMaterial({
+    uniforms: {
+      dispFactor: { type: "f", value: 0.0 },
+      currentImage: { type: "t", value: sliderImages[0] },
+      nextImage: { type: "t", value: sliderImages[1] } },
+
+    vertexShader: vertex,
+    fragmentShader: fragment,
+    transparent: true,
+    opacity: 1.0 });
+
+
+  let geometry = new THREE.PlaneBufferGeometry(
+  parent.offsetWidth,
+  parent.offsetHeight,
+  1);
+
+  let object = new THREE.Mesh(geometry, mat);
+  object.position.set(0, 0, 0);
+  scene.add(object);
+
+  let addEvents = function () {
+
+    let pagButtons = Array.from(document.getElementById('pagination').querySelectorAll('button'));
+    let isAnimating = false;
+
+    pagButtons.forEach(el => {
+
+      el.addEventListener('click', function () {
+
+        if (!isAnimating) {
+
+          isAnimating = true;
+
+          document.getElementById('pagination').querySelectorAll('.active')[0].className = '';
+          this.className = 'active';
+
+          let slideId = parseInt(this.dataset.slide, 10);
+
+          mat.uniforms.nextImage.value = sliderImages[slideId];
+          mat.uniforms.nextImage.needsUpdate = true;
+
+          TweenLite.to(mat.uniforms.dispFactor, 1, {
+            value: 1,
+            ease: 'Expo.easeInOut',
+            onComplete: function () {
+              mat.uniforms.currentImage.value = sliderImages[slideId];
+              mat.uniforms.currentImage.needsUpdate = true;
+              mat.uniforms.dispFactor.value = 0.0;
+              isAnimating = false;
+            } });
+
+
+          let slideTitleEl = document.getElementById('slide-title');
+          let slideStatusEl = document.getElementById('slide-status');
+          let nextSlideTitle = document.querySelectorAll(`[data-slide-title="${slideId}"]`)[0].innerHTML;
+          let nextSlideStatus = document.querySelectorAll(`[data-slide-status="${slideId}"]`)[0].innerHTML;
+
+          TweenLite.fromTo(slideTitleEl, 0.5,
+          {
+            autoAlpha: 1,
+            y: 0 },
+
+          {
+            autoAlpha: 0,
+            y: 20,
+            ease: 'Expo.easeIn',
+            onComplete: function () {
+              slideTitleEl.innerHTML = nextSlideTitle;
+
+              TweenLite.to(slideTitleEl, 0.5, {
+                autoAlpha: 1,
+                y: 0 });
+
+            } });
+
+
+          TweenLite.fromTo(slideStatusEl, 0.5,
+          {
+            autoAlpha: 1,
+            y: 0 },
+
+          {
+            autoAlpha: 0,
+            y: 20,
+            ease: 'Expo.easeIn',
+            onComplete: function () {
+              slideStatusEl.innerHTML = nextSlideStatus;
+
+              TweenLite.to(slideStatusEl, 0.5, {
+                autoAlpha: 1,
+                y: 0,
+                delay: 0.1 });
+
+            } });
+
+
+        }
+
+      });
+
+    });
+
+  };
+
+  addEvents();
+
+  window.addEventListener('resize', function (e) {
+    renderer.setSize(renderW, renderH);
+  });
+
+  let animate = function () {
+    requestAnimationFrame(animate);
+
+    renderer.render(scene, camera);
+  };
+  animate();
+};
+
+imagesLoaded(document.querySelectorAll('img'), () => {
+
+  document.body.classList.remove('loading');
+
+  const el = document.getElementById('slider');
+  const imgs = Array.from(el.querySelectorAll('img'));
+  new displacementSlider({
+    parent: el,
+    images: imgs });
+
+
+});
